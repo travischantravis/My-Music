c2f85a95e38611f84fc8b972e9056b50435d7f7f
travischantravis <chancheukyin4463@gmail.com>
2020-04-08 00:19:42 -0700

    added history

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ app.get("/login", function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  const scope = "user-read-private user-read-email user-top-read";
+  const scope =
+    "user-read-private user-read-email user-top-read user-read-recently-played";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
